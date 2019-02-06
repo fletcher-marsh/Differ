307721535a4b6e17a9b0a4ae9e236e6c820aa55a
Fletcher Marsh <fletcher.r.marsh@gmail.com>
2019-02-06 14:41:57 -0500

    Make setup script a bit more robust, arg capture for main

--- differ.py
+++ differ.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 
 from git import repo
+import sys
+
+
+
+def main():
+    if len(sys.argv) == 1:
+        print("Usage: differ [path/to/directory]")
+    else:
+        directory = sys.argv[1]
+        print(directory)
+
+
+if __name__ == "__main__":
+    main()
--- setup.sh
+++ setup.sh
@@ -1,2 +1,16 @@
+if [ -f /usr/local/bin/differ ]
+then
+    echo "Found a binary existing in /usr/local/bin named 'differ'. Do you want to overwrite it? (y/n): "
+    read answer
+    if [ "$answer" != "${answer#[Yy]}" ]; then
+        echo "Updating differ..."
+        rm /usr/local/bin/differ.py
+        rm /usr/local/bin/differ
+    else
+        echo "Canceling install."
+        exit 0
+    fi
+fi
 cp differ.py /usr/local/bin/differ.py
 ln -s /usr/local/bin/differ.py /usr/local/bin/differ
+echo "Done!"
